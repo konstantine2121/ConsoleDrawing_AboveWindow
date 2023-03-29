bc486bba5c5e81ef04a859ba4f131f151258f123
Konstantine <kach91kach91@gmail.com>
2023-03-29 23:49:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleDrawing_AboveWindow/AboveWindowDrawer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDrawing_AboveWindow
+{
+    internal class AboveWindowDrawer : IDisposable
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr DeleteDC(IntPtr hDc);
+
+        private static IntPtr hWnd = IntPtr.Zero;
+        private static IntPtr hDC = IntPtr.Zero;
+        private static Graphics consoleGraphics = null;
+
+
+        private void FullDrawExample()
+        {
+            hWnd = GetConsoleWindow();
+            if (hWnd != IntPtr.Zero)
+            {
+                hDC = GetDC(hWnd);
+                if (hDC != IntPtr.Zero)
+                {
+                    using (Graphics consoleGraphics = Graphics.FromHdc(hDC))
+                    {
+
+                        Pen whitePen = new Pen(Color.White);
+                        Pen redPen = new Pen(Color.Red, 2);
+                        Font font = new Font("verdana", 13);
+
+                        consoleGraphics.DrawLine(whitePen, new Point(0, 30), new Point(300, 30));
+                        consoleGraphics.DrawLine(whitePen, new Point(30, 0), new Point(30, 300));
+                        consoleGraphics.DrawString("X", font, Brushes.White, 310, 10);
+                        consoleGraphics.DrawString("Y", font, Brushes.White, 40, 250);
+                        consoleGraphics.DrawString("График функции : y = x + 2", font, Brushes.White, 80, 60);
+
+                        int y = 0;
+                        int x = 100;
+                        for (; x < 250; x++)
+                        {
+                            y = x + 2;
+                            consoleGraphics.DrawRectangle(redPen, x, y, 2, 2);
+                        }
+
+                        font.Dispose();
+                        whitePen.Dispose();
+                        redPen.Dispose();
+                    }
+
+                }
+                ReleaseDC(hWnd, hDC);
+                DeleteDC(hDC);
+
+            }
+
+            Console.ReadKey();
+        }
+
+        public bool StartDrawing(out Graphics graphics)
+        {
+            Dispose();
+
+            graphics = null;
+
+            hWnd = GetConsoleWindow();
+            if (hWnd == IntPtr.Zero)
+            {
+                return false;
+            }
+            
+            hDC = GetDC(hWnd);
+            
+            if (hDC == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            consoleGraphics = Graphics.FromHdc(hDC);
+            graphics = consoleGraphics;
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            if (hWnd == IntPtr.Zero || hDC == IntPtr.Zero)
+            {
+                return;
+            }
+
+            consoleGraphics.Dispose();
+            ReleaseDC(hWnd, hDC);
+            DeleteDC(hDC);
+        }
+    }
+}
---
+++ ConsoleDrawing_AboveWindow/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleDrawing_AboveWindow
+{
+    internal class Program
+    {
+        const int MaxWidth = 700;
+        const int MaxHeight = 500;
+        private static Random _random = new Random();
+        private static Image _fish = LoadFish();
+
+        public static Image Fish { get { return _fish.Clone() as Image; } }
+
+        static void Main(string[] args)
+        {
+            var drawer = new AboveWindowDrawer();
+            
+            Graphics graphics = null;
+
+            if (! drawer.StartDrawing(out graphics))
+            {
+                Console.WriteLine("Контекст рисования не запущен.");
+                Console.ReadKey();
+                return;
+            }
+
+            var amountOfFishes = 20;
+
+            for (int i = 0;  i < amountOfFishes; i++)
+            {
+                DrawRandomFish(graphics);
+            }
+
+            Console.ReadKey();
+            drawer.Dispose();
+        }
+
+        static Point GetNewRandomPoint()
+        {
+            return new Point(_random.Next(MaxWidth), _random.Next(MaxHeight));
+        }
+
+        static bool GetNewRandomTurn()
+        {
+            return _random.Next(2) == 1;
+        }
+
+        static Size GetNewRandomSize()
+        {
+            var max = 4;
+            var min = 0.2f;
+            var value = _random.NextDouble() * max;
+
+            if (value < min)
+            {
+                value = min;
+            }
+
+            int width = Fish.Size.Width * (int)value;
+            int height = Fish.Size.Height * (int)value;
+
+            return new Size(width, height);
+        }
+
+        static void DrawRandomFish(Graphics graphics)
+        {
+            DrawFish(graphics, GetNewRandomPoint(), GetNewRandomTurn(), GetNewRandomSize());
+        }
+
+        static void DrawFish(Graphics graphics, Point fishLocation, bool left, Size size)
+        {
+            var fish = Fish;
+            if (left)
+            {                
+                fish.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            }
+            if (size == Size.Empty)
+            {
+                size = fish.Size;
+            }
+
+            graphics.DrawImage(fish, fishLocation.X, fishLocation.Y, size.Width, size.Height);
+        }
+
+        static Image LoadFish() 
+        {
+            var path = "Images\\fish.png";
+
+            return new Bitmap(path);
+        }
+    }
+}
